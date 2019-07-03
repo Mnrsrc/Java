0816f5ba8f723c156b6f3fb9032659a98c6cdaea
Munire <51300375+Mnrsrc@users.noreply.github.com>
2019-07-03 20:21:57 +0300

    Add files via upload

---
+++ While_Counter.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package while_counter;
+
+/**
+ *
+ * @author Munire
+ */
+//import javax.swing;swing
+
+
+public class While_Counter {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        int counter = 0;
+        System.out.println("Counter with While Loop: \n");
+        while (counter <= 10) {
+
+            counter = counter + 1;
+            System.out.println(counter);
+        }
+
+    }
+
+}
